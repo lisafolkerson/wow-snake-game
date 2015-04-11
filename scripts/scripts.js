f7c02111741ae6d78971a71f1cca2906a92d77de
Lisa Folkerson <lisafolkerson@gmail.com>
2015-04-10 20:08:43 -0400

    pseudo code writ

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+// Set Up the Canvas
+// Set the score to 0
+// create the snake
+// create the apple
+
+
+Every 1000 milliseconds {
+	clear the Canvas
+	draw current score on screen
+	move snake in current direction
+
+	if (snake collides with wall or itself) {
+		end the game
+	} else if (snake eats an apple){
+		add one to score
+		move apple to new location
+		make snake longer
+	}
+	For (each segment of the snake) {
+		draw the segment
+	}
+	draw apple
+	draw border
+}
+
+When the user presses a key {
+	if (the key is an arrow)
+}
+}
